078ad70add6b3b9ab6bccffc1aaace4283caebc0
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2025-09-11 22:04:01 +0530

    updated navlist component with rout links

--- src/app/components/nav-list/nav-list.ts
+++ src/app/components/nav-list/nav-list.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-nav-list',
-  imports: [MatListModule, MatRippleModule, MatIconModule],
+  imports: [MatListModule, MatRippleModule, MatIconModule, RouterModule],
   templateUrl: './nav-list.html',
   styleUrl: './nav-list.scss'
 })
